63801ca0e670e4159331c58f823ab94a5f3b80f2
SilvaDenisVictor <108144928+SilvaDenisVictor@users.noreply.github.com>
2022-09-16 16:44:21 -0300

    Create 03.cpp

---
+++ 03.cpp
@@ -0,0 +1,63 @@
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void min_hipfy(vector<double>& vetor, int posicao, int inicio, int fim);
+
+void Heap_sort(vector<double>& vetor, int inicio, int fim);
+       
+
+int main()
+{
+    vector<double> vetor = {10,9,8,7,6,5,4,3,2,1,0};
+ 
+    for(int c = 0; c < vetor.size(); c++){
+        cout << vetor[c] << " ";
+    }
+    
+    Heap_sort(vetor, 0, vetor.size()-1);
+    cout << endl;
+    
+    for(int c = 0; c < vetor.size(); c++){
+        cout << vetor[c] << " ";
+    }
+    
+    return 0;
+}
+
+void trocar(double& x, double& y){
+    double z = x;
+    x = y;
+    y = z;
+}
+
+void min_hipfy(vector<double>& vetor, int posicao, int inicio, int fim){
+    int Esq = 2*(posicao - inicio + 1)+ inicio - 1;
+    int Dir = Esq + 1;
+    int menor = posicao;
+    
+    if(Esq <= fim && vetor[posicao] > vetor[Esq]) menor = Esq;
+    if(Dir <= fim && vetor[menor] > vetor[Dir]) menor = Dir;
+    if(posicao != menor){
+        trocar(vetor[posicao], vetor[menor]);
+        min_hipfy(vetor, menor, inicio, fim);
+    }
+    
+}
+
+void Heap_sort(vector<double>& vetor, int inicio, int fim){  
+    for(int c = (fim + inicio - 1)/2; c >= inicio; c--){//(posicao - inicio + 1)/2+ inicio - 1
+        min_hipfy(vetor, c, inicio, fim);
+    }
+    
+    for(int c = fim; c < inicio; c--){
+        trocar(vetor[inicio], vetor[fim]);
+        
+        fim = fim - 1;
+        min_hipfy(vetor, inicio, inicio, fim);
+    }
+}
+
+
